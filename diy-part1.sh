1790949fdca4b7d6a986afefcef6ba5ebc1b0331
17688838786 <1344352740@qq.com>
2026-08-30 14:18:06 +0800

    Fix QModem config: properly disable old version, enable next version

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,20 +17,20 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# 缁夊娅庡鎻掔摠閸︺劎娈憅modem feed閿涘牓浼╅崗宥夊櫢婢跺稄绱?
+# 缂佸顭峰▍搴☆啅閹绘帞鎽犻柛锔哄妿濞堟唴modem feed闁挎稑鐗撴导鈺呭礂瀹ュ娅㈠璺虹▌缁?
 sed -i '/src-git qmodem/d' feeds.conf.default
 sed -i '/src-git openclash/d' feeds.conf.default
 
-# QModem - 5G濡紕绮嶇粻锛勬倞閿涘牆鎯堥惌顓濅繆閵嗕礁顦縒AN閵嗕箑TL閸旂喕鍏橀敍?
+# QModem - 5G婵☆垽绱曠划宥囩不閿涘嫭鍊為柨娑樼墕閹牓鎯岄婵呯箚闁靛棔绀侀ˇ绺扐N闁靛棔绠慣L闁告梻鍠曢崗姗€鏁?
 echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >>feeds.conf.default
 
-# OpenClash - 缂冩垹绮跺銉ュ徔
+# OpenClash - 缂傚啯鍨圭划璺侯啅閵夈儱寰?
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
 
-# LuCI娑撳顣?- argon-mod (閸╄桨绨崢鐔哄argon娴兼ê瀵?
+# LuCI濞戞挸顭烽。?- argon-mod (闁糕晞妗ㄧ花顒勫储閻斿搫顣糰rgon濞村吋锚鐎?
 rm -rf package/lean/luci-theme-argon-mod
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-mod 2>/dev/null || true
 
-# LuCI娑撳顣?- neobird (娑撴挷璐熺粔璇插З缁旑垯绱崠?
+# LuCI濞戞挸顭烽。?- neobird (濞戞挻鎸风拹鐔虹矓鐠囨彃袟缂佹棏鍨槐顓㈠礌?
 rm -rf package/lean/luci-theme-neobird
 git clone --depth=1 https://github.com/thinktip/luci-theme-neobird.git package/lean/luci-theme-neobird 2>/dev/null || true
